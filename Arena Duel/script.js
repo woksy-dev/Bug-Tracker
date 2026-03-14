e2ead914850b1f8264be49e2820ab4f87c4614d5
Robin <Woksy@hotmail.com>
2026-03-14 13:37:22 +0100

    Added basic features, like renderFighters and addEventListener on submitBtn

--- Arena Duel/script.js
+++ Arena Duel/script.js
@@ -8,3 +8,51 @@ const deadBtn = document.querySelector("#showDead");
 const fightRoundBtn = document.querySelector("#fightRound");
 const resetArenaBtn = document.querySelector("#resetArena");
 const fighterList = document.querySelector("#fighterList");
+
+
+let fighters = [];
+
+
+function addFighter (name, power) {
+
+    const fighter = {
+
+        id: Date.now(),
+        name,
+        power,
+        hp: 100,
+        dead: false
+    }
+
+    fighters.push(fighter);
+}
+
+function renderFighters() {
+
+    fighterList.innerHTML = "";
+
+    for (const f of fighters) {
+
+        let li = document.createElement("li");
+        let status = f.dead ? "Dead" : "Alive";
+        li.textContent = `${f.name} // HP: ${f.hp} // ${status}`;
+
+        fighterList.appendChild(li);
+    }
+}
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let name = nameInput.value.trim();
+    let power = Number(attackInput.value);
+
+    addFighter(name, power)
+    renderFighters();
+    
+    form.reset()
+    nameInput.focus()
+});
+
+renderFighters();
